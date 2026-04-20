8632541e330eb531b009a1560cc83203c657772e
Kyle Seguin <kylseg@outlook.com>
2026-04-19 20:39:36 -0400

    Initial Project creation Commit

---
+++ Source/LaserHallway/LaserHallway.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LaserHallway.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LaserHallway, "LaserHallway" );
